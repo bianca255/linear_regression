2734c1f1e2fc92b53318b951cce17166aa8e3db9
bianca255 <b.haguma@alustudent.com>
2026-03-24 23:38:36 +0300

    Set Flutter API URL to Render endpoint

--- linear_regression_model/summative/FlutterApp/lib/main.dart
+++ linear_regression_model/summative/FlutterApp/lib/main.dart
@@ -54,7 +54,7 @@ class PredictionPage extends StatefulWidget {
 
 class _PredictionPageState extends State<PredictionPage> {
   // Replace with your Render URL after deployment
-  static const String apiUrl = 'https://your-app-name.onrender.com/predict';
+  static const String apiUrl = 'https://student-math-api.onrender.com/predict';
 
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
